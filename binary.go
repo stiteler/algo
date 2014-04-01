641e424d74070cae1ea01558b232ee3de612aa06
steaz <cstiteler@comcast.net>
2014-04-01 19:59:53 -0400

    formatting

--- binary.go
+++ binary.go
@@ -9,12 +9,10 @@ func main() {
 	hay := []int{3, 4, 2, 34, 2, 5, 4, 5, 6, 7, 8, 9, 6, 5, 6, 7, 7, 6 , 5, 4, 3, 54}
 	//sort it (test mergesort)
 	algo.Mergesort(hay)
-	fmt.Println(hay)
 
 	ndl := 1 //won't be found
 	ndl2 := 2 //will be found
 
-	
 	fmt.Println(BinarySearch(ndl, hay))
 	fmt.Println(BinarySearch(ndl2, hay))
 
@@ -25,8 +23,8 @@ func main() {
 func BinarySearch(ndl int, hay []int) bool {
 	//calculate mid everytime. 
 	mid := int(len(hay)/2)
-	//we need thhis because 
-	if(mid < 1) { return false }
+	//we need this to check previous slice wasn't empty, or size 1.
+	if mid < 1 { return false }
 
 	if ndl == hay[mid] {
 		return true
